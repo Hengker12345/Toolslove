24d14504a7b4c2c564e810cc97b4c5403733d9a8
Hengker EpEp <167304514+THEOYS123@users.noreply.github.com>
2024-05-11 10:02:15 +0700

    Update love.sh

--- love.sh
+++ love.sh
@@ -1,72 +1,622 @@
-#Toolslove
-#Ren
+#Linux999
+#Debian
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
 
+while true; do
+read -p "username  : " l love you
+read -p "Password  :  " no l love you
+if [[ "$l love you" = "Ren9999" || "$no l love you" = "janda muda🗿" ]]; then
 
-echo -e $red"
+else
+echo "Anda Bukan player epep🗿"
+exit
+fi
+done
 sleep 1
 clear
-sleep 1        echo -e $red"
-echo -e "  ███████████████████████████████"
-echo -e "  ████╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬████"
-echo -e "  ██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬███████╬╬╬╬╬╬╬╬╬███████╬╬╬█"
-echo -e "  █╬╬██╬╬╬╬███╬╬╬╬╬╬╬███╬╬╬╬██╬╬█"
-echo -e "  █╬██╬╬╬╬╬╬╬██╬╬╬╬╬██╬╬╬╬╬╬╬██╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬╬█████╬╬╬╬╬╬╬╬╬╬╬█████╬╬╬╬█"
-echo -e "  █╬╬█████████╬╬╬╬╬╬╬█████████╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬▓▓▓▓╬╬╬╬╬╬╬█╬╬╬╬╬╬╬▓▓▓▓╬╬╬█"
-echo -e "  █╬╬▓▓▓▓▓▓╬╬█╬╬╬█╬╬╬█╬╬▓▓▓▓▓▓╬╬█"
-echo -e "  █╬╬╬▓▓▓▓╬╬██╬╬╬█╬╬╬██╬╬▓▓▓▓╬╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬██╬╬╬╬█╬╬╬╬██╬╬╬╬╬╬╬╬█"
-echo -e "  █╬╬╬╬╬████╬╬╬╬███╬╬╬╬████╬╬╬╬╬█"
-echo -e "  █╬╬╬╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬╬╬╬█"
-echo -e "  ██╬╬█╬╬╬╬╬╬╬╬█████╬╬╬╬╬╬╬╬█╬╬██"
-echo -e "  ██╬╬██╬╬╬╬╬╬███████╬╬╬╬╬╬██╬╬██"
-echo -e "  ██╬╬▓███╬╬╬████╬████╬╬╬███▓╬╬██"
-echo -e "  ███╬╬▓▓███████╬╬╬███████▓▓╬╬███"
-echo -e "  ███╬╬╬╬▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬███"
-echo -e "  ████╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬████"
-echo -e "  █████╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬█████"
-echo -e "  ██████╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬██████"
-echo -e "  ███████╬╬╬╬╬╬╬███╬╬╬╬╬╬╬███████"
-echo -e "  ████████╬╬╬╬╬╬███╬╬╬╬╬╬████████"
-echo -e "  █████████╬╬╬╬╬███╬╬╬╬╬█████████"
-echo -e "  ███████████╬╬╬╬█╬╬╬╬███████████"
-echo -e "  ███████████████████████████████"
-
-
-
-
-echo -e "    
+sleep 1
+echo -e "  ████████╗░█████╗░░█████╗░██╗░░░░░░██████╗░░██╗██╗░░██╗██╗░░██╗██╗"
+echo -e "  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝░██╔╝██║░██╔╝██║░██╔╝██║"
+echo -e "  ░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░██╔╝░██║██╔╝░██║██╔╝░██║"
+echo -e "  ░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗███████║███████║███████║"
+echo -e "  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝╚════██║╚════██║╚════██║"
+echo -e "  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░░░░░░╚═╝░░░░░╚═╝░░░░░╚═╝"
+echo -e " "
 echo
-read -p "          PILIH MENU:" HOOH
-if [ $HOOH == 1 ]
-
-echo -e $blue "
-_______________________________________________"
-|             TOOLS ANONYMOUS                 |"
-|                 [2024]                      |"
-|_____________________________________________|"
-|1  |.DDOS WIFI JARINGAN MU KE PUTUS          |"
-|2  |.HACK PISZHER  01                        |"
-|3  |.HACK PIZHER   02                        |"
-|4  |.HACK PISZHER  03                        |"
-|5  |.HACK PIZHER   04                        |"
-|6  |.HACK PIZHER   05                        |"
-|7  |.HACK PISZHER  06                        |"
-|8  |.HACK PISZHER  07                        |"
-|9  |.HACK PISZHER  08                        |"
-|10 |.HACK PISZHER  09                        |"
-|11 |.HACK CAMERA PISZHER  01                 |"
-
-
-echo -e "    
+echo -e "                          By: Ren9999 ☕🗿"
+echo -e "                ________________________________________"
+echo -e "               |         𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐓𝐎𝐎𝐋𝐒𝟒𝟒𝟒          |"
+echo -e "               |                                        |"
+echo -e "               __________________________________________"
+echo -e "               |                                        |"
+echo -e "               |1.DDOS                2.BOT WA          |"
+echo -e "               |3.SPAM                4.NETHUNTER M     |"
+echo -e "               |5.BANED WA            6.TERMUX LXQT     |"
+echo -e "               |7.UNBANED WA          8.ELITE2          |"
+echo -e "               |9.HACK SOSMED         10.VIPER          |"
+echo -e "               |11.SPLOIT-MALICIOUS   12.SPAMM          |"
+echo -e "               |13.DDOS R             14.Tools999       |"
+echo -e "               |15.DDOS WEB           16.Tools Black    |"
+echo -e "               |17.hack CCTV          18.SpamWa         |"
+echo -e "               |19.kamera phising     20.ubuntu-mod     |"
+echo -e "               |21.KARMA-DDoS         22.SPAM           |"
+echo -e "               |23.owlsint            24.tools bajingan |"
+echo -e "               |25.Spam               26.spam           |"
+echo -e "               |27.spam               28.DIEJOUBU       |"
+echo -e "               |29.LAZYMUX            30.HACK PIZHER    |"
+echo -e "               |31.bot WA             32.Followes tiktok|"
+echo -e "               |33.champhising        34.spam otp       |"
+echo -e "               |35.DDOS               36.DDOS           |"
+echo -e "               |37.OSGPT              38.Virtext WA     |"
+echo -e "               |39.DDOS               40.DDOS W         |"
+echo -e "               |41.deface             42.ip finder      |"
+echo -e "               |43.NETHUNTER          44.zefbot         |"
+echo -e "               |________________________________________|"
+echo -e "               | UPDATE                                 |"
+echo -e "               | FOLLOW                                 |"
+echo -e "               | LAPORKAN BUG                           |"
+echo -e "               |________________________________________|"
 echo
-read -p "          PILIH MENU:" HOOH
+read -p "               PILIH MENU:" HOOH
 if [ $HOOH == 1 ]
+then
+clear
+sleep 1
+sudo apt-get update && sudo apt-get updgrade
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install curl
+sudo apt install git
+git clone https://github.com/Whomrx666/anonymous-c2
+cd anonymous-c2
+pip3 install -r requirements.txt
+python3 anonymous-c2.py
+fi
+
+if [ $HOOH == 3 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install  git
+ python -m pip install requests
+ git clone https://github.com/RozhBasXYZ/OTP
+ cd OTP
+ python spam.py
+fi
+
+if [ $HOOH == 5 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/152fTXoG9IEsp1unLH47Ap1mV3AvpsYYX/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 7 ]
+then
+clear
+sleep 1
+xdg-open "https://drive.google.com/file/d/14V3b11fDLTNzddSKTdVZiNlDwK7JBTjD/view?usp=drivesdk"
+fi
+
+if [ $HOOH == 9 ]
+then
+clear
+sleep 1
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+
+if [ $HOOH == 11 ]
+then
+clear
+sleep 1
+apt-get install git
+apt-get install python2 ruby
+gem install lolcat
+git clone https://github.com/TheSploit/Sploit-Malicious
+cd Sploit-Malicious
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $HOOH == 2 ]
+then
+clear
+sleep 1
+xdg-open "https://www.mediafire.com/file/f52nygbxa8kb580/node_modules.zip/file"
+fi
+
+if [ $HOOH == 4 ]
+then
+clear
+sleep 1
+pkg install root-repo 
+ pkg install x11-repo
+ apt update && apt upgrade -y
+ apt install wget -y 
+ wget https://raw.githubusercontent.com/shahinuralamin/Technical_Shahinur_Al-Amin/main/install-nethunter-modified 
+chmod +x install-nethunter-modified
+./install-nethunter-modified
+fi
+
+if [ $HOOH == 6 ]
+then
+clear
+sleep 1
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+git clone --depth 1 https://github.com/Yisus7u7/termux-desktop-lxqt 
+cd termux-desktop-lxqt 
+bash install-desktop-lxqt.sh
+start-desktop
+fi
+
+if [ $HOOH == 8 ]
+then
+clear
+sleep 1
+pkg update -y && pkg upgrade -y
+pkg install git python
+git clone --depth 1 https://github.com/Dapunta/elite2
+cd elite2
+git pull
+pip install -r requirements.txt
+python run.py
+fi
+
+if [ $HOOH == 10 ]
+then
+clear
+sleep 1
+apt update 
+apt upgrade 
+pkg install git 
+pkg install python
+git clone https://github.com/Moajjem404/VZERO
+cd VZERO
+pip install -r requirements.txt
+python main.py
+fi
+
+if [ $HOOH == 12 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+git clone https://github.com/AmmarrBN/Tools-WhatsApp
+bash install.sh
+cd Tools-WhatsApp
+python main.py
+fi
+
+if [ $HOOH == 13 ]
+then
+clear
+sleep 1
+pkg install git -y
+pkg install python -y
+pkg install python3 -y
+git clone https://github.com/palahsu/DDoS-Ripper.git
+cd DDoS-Ripper 
+ls
+python3 DRipper.py
+fi
+
+if [ $HOOH == 14 ]
+then
+clear
+sleep 1
+git clone https://github.com/SPOKYHOZOOO/TOOLS999 
+cd TOOLS999
+bash TOOLS999.sh
+fi
+
+if [ $HOOH == 15 ]
+then
+clear
+sleep 1
+apt update
+apt upgrade
+pkg install python3 git -y
+git clone https://github.com/kdo2064/DxCyberD
+cd DxCyberD
+python3 dx.py
+python3 dx-web.py
+fi
+
+if [ $HOOH == 16 ]
+then
+clear
+sleep 1
+pkg update
+pkg install git
+pkg install bash
+git clone https://github.com/MRHOOH/BLACKV1
+cd BLACKV1
+bash BLACKV1.sh
+fi
+
+if [ $HOOH == 17 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade 
+git clone https://github.com/mohammadmahdi-termux/hackCCTV 
+cd hackCCTV 
+python3 hackcctv.py
+fi 
+
+if [ $HOOH == 18 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install git
+python3 -m pip install requests
+git clone https://github.com/krypton-byte/SpamWa
+cd SpamWa
+python3 spam.py
+fi
+
+if [ $HOOH == 19 ]
+then
+clear
+sleep 1
+git clone https://github.com/jasut1n/WebCamPhishing
+cd WebCamPhishing
+chmod +x webcamphish.sh
+./webcamphish.sh
+fi
+
+if [ $HOOH == 20 ]
+then
+clear
+sleep 1
+git clone https://github.com/Hozo999/ubuntu-mod
+cd ubuntu-mod
+chmod 777 ubuntu-mod
+bash ubuntu-mod.sh
+ubuntu
+desktop-mod.sh
+fi
+
+if [ $HOOH == 21 ]
+then
+clear
+sleep 1
+git clone https://github.com/HyukIsBack/KARMA-DDoS.git
+cd KARMA-DDoS
+pip3 install -r requirements.txt
+python3 main.py
+fi
+
+if [ $HOOH == 22 ]
+then
+clear
+sleep1
+apt update && apt upgrade
+apt install git -y
+apt install python3-pip
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip3 install -r install.txt
+python3 brutal.py
+fi
+
+if [ $HOOH == 23 ]
+then
+clear
+sleep1
+pkg update && pkg upgrade -y
+apt list --upgradable 
+pkg install python -y
+Pkg install git -y
+git clone https://github.com/IccTeam/Owl-sint
+Install module
+pip install colorama
+pip2 install colorama
+pip install requests
+pip2 install requests
+pip install phonenumbers
+pip2 install phonenumbers
+pip install datetime
+pip2 install datetime
+pip3 install --upgrade instaloader 
+echo -e "\u001b[32mDone installing pip!"
+echo -e "Running Tools..."
+sleep 5
+python owlsint.py
+fi
+
+if [ $HOOH == 24 ]
+then
+clear
+sleep1
+echo -e "mama BAJINGAN"
+echo -e "pw Gans    [enter]"
+apt install update
+apt install upgrade
+pip2 install termcolor
+pip2 install lolcat
+apt install toilet
+apt install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $HOOH == 25 ]
+then
+clear
+sleep 1
+apt upgrade && apt update
+apt install git
+git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh install.sh
+sh sdv.sh
+fi
+
+if [ $HOOH == 26 ]
+then
+clear
+sleep 1
+pkg update
+pkg upgrage
+pkg install python
+pkg install python-pip
+pkg install git
+pip install requests
+pip install rich
+git clone https://github.com/TZSEC7/TZspam
+cd TZspam
+python TZsec7.py
+Spam
+fi
+
+if [ $HOOH == 27 ]
+then
+clear
+sleep 1
+cd $HOME
+apt update && apt upgrade
+pkg install bash curl wget jq
+pkg install python python-pip
+pkg install make clang
+pkg install git
+git clone https://github.com/Sxp-ID/Brutal-W4
+ cd Brutal-W4
+git pull
+make install
+./main
+fi
+
+if [ $HOOH == 28 ]
+then
+clear
+sleep 1
+pkg upgrade && pkg update
+pkg install php
+pkg install git
+gitclone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php daijobu.php
+fi
+
+if [ $HOOH == 29 ]
+then
+clear
+sleep 1
+pkg update && upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python lazymux.py
+fi
+
+if [ $HOOH == 30 ]
+then
+clear
+sleep 1
+git clone https://github.com/XPH4N70M/XPHISHER.git
+cd XPHISHER 
+bash xphisher.sh
+fi
+
+if [ $HOOH == 31 ]
+then
+clear
+sleep 1
+git clone https://github.com/aydinnyunus/WhatsappBOT.git
+cd WhatsappBOT
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python WhatsappBot.py
+fi 
+
+if [ $HOOH == 32 ]
+then
+clear
+sleep 1
+apt update -y && apt upgrade -y
+pkg install git python-pip
+git clone https://github.com/RozhakXD/PubTok
+cd PubTok
+python -m pip install -r requirements.txt
+uname -m
+chmod +x aarch64
+./aarch64
+fi
+
+if [ $HOOH == 33 ]
+then
+clear
+sleep 1
+apt update && apt upgrade -y
+apt install git -y
+apt install php
+apt install curl -y
+apt install wget -y
+git clone https://github.com/XPH4N70M/HACK-CAMERA.git
+cd HACK-CAMERA
+bash setup
+bash hack_camera.sh
+fi
+
+if [ $HOOH == 34 ]
+then
+clear
+sleep 1
+apt update -y && apt upgrade -y
+apt install git -y
+apt install python -y
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip install -r install.txt
+python brutal.py
+fi
+
+if [ $HOOH == 35 ]
+then
+clear
+sleep 1
+git clone https://github.com/karthik558/ddos-attack.git 
+cd ddos-attack 
+pip install -r requirements.txt
+python3 ddos.py
+fi
+
+if [ $HOOH == 36 ]
+then
+clear
+sleep 1
+git clone https://github.com/mrprogrammer2938/DDos-Attack.git
+cd DDos-attack
+bash install.sh
+python DDos.py
+fi
+
+if [ $HOOH == 37 ]
+then
+clear
+sleep 1
+git clone https://github.com/Ravi-Teja-konda/OSGPT.git 
+cd OSGPT
+pip install -r requirements.txt
+python3 main.py
+fi
+
+if [ $HOOH == 38 ]
+then
+clear
+sleep 1
+xdg-open "https://www.mediafire.com/file/2gfa9gekyhcihgj/Virtex_WA_Slayer.txt/file"
+fi
+
+if [ $HOOH == 39 ]
+then
+clear
+sleep 1
+git clone https://github.com/MIISTERC/DDos-Blitz
+cd DDos-Blitz
+bash setup.sh
+fi
+
+if [ $HOOH == 40 ]
+then
+clear
+sleep 1
+git clone https://github.com/thenurhabib/warlof.git
+cd warlof
+bash setup.sh
+python3 warlof.py --help
+fi
+
+if [ $HOOH == 41 ]
+then
+clear
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone http://github.com/nfs-tech-bd/auto-deface
+cd auto-deface
+python2 auto-deface.py
+fi
+
+if [ $HOOH == 42 ]
+then
+clear
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone http://github.com/nfs-tech-bd/IP-FINDER
+cd IP-FINDER
+python2 ip-finder.py
+fi
+
+if [ $HOOH == 43 ]
+then
+clear
+termux-setup-storage
+pkg update
+pkg upgrade
+pkg install git
+pkg install wget
+git clone https://github.com/Hozo999/NETHUNTER
+cd NETHUNTER
+bash NETHUNTER.sh
+fi
+
+if [ $HOOH == 44 ]
+then
+clear
+pkg upgrade && pkg update
+pkg install git
+pip install requests
+pip install bs4
+git clone https://github.com/sptty-chan/zefbot
+cd zefbot
+python zefbot.py
+fi
+
+if [ $HOOH == UPDATE ]
+then
+clear
+sleep 1
+git clone https://github.com/THEOYS123/ToolsRen.git
+cd ToolsRen
+bash ren999.sh
+fi
+
+if [ $HOOH == FOLLOW ]
+then
+clear
+sleep 1
+xdg-open "https://www.tiktok.com/@sistem9999?_t=8mE62usv4Br&_r=1"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+fi
+
+if [ $HOOH == LAPORKAN BUG ]
+then
+clear
+sleep 1
+xdg-open "https://wa.me/6289519450908"
+fi
